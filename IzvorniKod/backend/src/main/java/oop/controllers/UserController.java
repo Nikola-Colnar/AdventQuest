1ac21bdda12ce240f4635a7691eb9c9d5af768a4
Marko Grgurić <marko.grguric2@fer.unizg.hr>
2024-11-06 15:27:57 +0100

    Dodan UserController u backhandu za dohvat i slanje podataka s frontenda get/post metodom. U frontendu dodane privremenih forma za testiranje komunikacije front-back

---
+++ IzvorniKod/backend/src/main/java/oop/controllers/UserController.java
@@ -0,0 +1,40 @@
+package oop.controllers;
+
+import oop.model.User;
+import oop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@CrossOrigin(origins="http://localhost:5174/")
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        // Spremanje korisnika
+        User createdUser = userService.createUser(user);
+
+        // Vraća korisnika u odgovoru sa statusom 201 (Created)
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        // Dohvaća sve korisnike
+        List<User> users = userService.getAllUsers();
+
+        // Vraća listu korisnika sa statusom 200 (OK)
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+}
