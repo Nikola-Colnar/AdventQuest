03e29117f1607f31da3db7be42023d3dd4d34580
Marko Grgurić <marko.grguric2@fer.unizg.hr>
2024-11-08 00:56:28 +0100

    Dodan je gumb za registraciju, u njemu se moze birati izmedu korisnika i predstavnika, izgled jos nije gotov ali ce se prilagodavati, sada imamo odvojene forme za sign in i login

--- IzvorniKod/frontend/src/App.jsx
+++ IzvorniKod/frontend/src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import ListaKorisnika from './ListaKorisnika'
 import Form from './Form'
+import RegForm from './RegForm'
 import Countdown from './Countdown'
 import Snowfall from './Snowfall'
 import LoginButton from './LoginButton'
+import SignupButton from './SignupButton'
+
 
 function App() {
   const [isFormVisible, setIsFormVisible] = useState(false); //Stanje vidljivosti login forme
@@ -12,13 +15,21 @@ function App() {
   const showForm = () => setIsFormVisible(true); // prikazuje formu
   const hideForm = () => setIsFormVisible(false); // Sakriva formu
 
+  const [isRegFormVisible, setIsRegFormVisible] = useState(false); //Stanje vidljivosti login forme
+
+  const showRegForm = () => setIsRegFormVisible(true); // prikazuje formu
+  const hideRegForm = () => setIsRegFormVisible(false); // Sakriva formu
+
   const [count, setCount] = useState(0)
 
   return (
     <>
       <LoginButton onClick={showForm}/>
-      <Snowfall/>
       {isFormVisible && <Form onClick={hideForm} />}
+      <SignupButton onClick={showRegForm}/>
+      {isRegFormVisible && <RegForm onClick={hideRegForm} />}
+      <Snowfall/>
+      
       <Countdown targetDate="2024-12-25T00:00:00"/>
     </>
   )
