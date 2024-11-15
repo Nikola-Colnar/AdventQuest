4679896c8f0c75ce1a447c72d1a5219d4686b7c4
Karlo Matanic <karlo.matanic@fer.unizg.hr>
2024-11-15 02:37:26 +0100

    Reformatiran AuthContext.jsx i firebaseConfig.js
    - ovo je zadnji commit za reformatiranje
    - cijeli frontend sada ima konstantan stil s 2 space indent + prettier

--- IzvorniKod/frontend/src/Firebase/AuthContext.jsx
+++ IzvorniKod/frontend/src/Firebase/AuthContext.jsx
@@ -1,24 +1,29 @@
 import React, { useContext, useEffect, useState } from "react";
 import { auth } from "./firebaseConfig.js";
 import { onAuthStateChanged } from "firebase/auth";
+import PropTypes from "prop-types";
+
 
 const AuthContext = React.createContext(null);
 
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null);
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            setCurrentUser(user);
-        });
-        return unsubscribe;
-    }, []);
+  useEffect(() => {
+    return onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+  }, []);
 
-    return (
-        <AuthContext.Provider value={{ currentUser }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
