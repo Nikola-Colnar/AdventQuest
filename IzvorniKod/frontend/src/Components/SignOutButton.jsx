3bdf2303d9dd08b58f4cfe710932a7e36a61d98d
Karlo Matanic <karlo.matanic@fer.unizg.hr>
2024-11-15 02:15:33 +0100

    Reformatiran Header i SignOutButton jsx

--- IzvorniKod/frontend/src/Components/SignOutButton.jsx
+++ IzvorniKod/frontend/src/Components/SignOutButton.jsx
@@ -1,45 +1,53 @@
-import React from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase/firebaseConfig.js";
 import { useAuth } from "../firebase/AuthContext";
-import {styled} from "@mui/system";
-import {MenuItem} from "@mui/material";
+import { styled } from "@mui/system";
+import { MenuItem } from "@mui/material";
+import PropTypes from "prop-types";
 
-const SignOutButton = ({onClick,onClose}) => {
-    const { currentUser } = useAuth();
 
-    const handleSignOut = () => {
-        signOut(auth)
-          .then(() => {
-              alert("You have signed out successfully!");
-              localStorage.clear()
-              onClick();
-              onClose();
-          })
-          .catch((error) => {
-              console.error("Error signing out: ", error);
-          });
-    };
+const SignOutButton = ({ onClick, onClose }) => {
+  const { currentUser } = useAuth();
 
-    const LogoutButton = styled(MenuItem)(() => ({
-        backgroundColor: 'rgba(255, 24, 24, 0.27)',  // osnovna pozadina
-        color: 'black',  // osnovna boja teksta
-        textAlign: 'center',
-        justifyContent: 'center',
+  // funkcija za sign out
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        alert("You have signed out successfully!");
+        localStorage.clear();
+        onClick();
+        onClose();
+      })
+      .catch((error) => {
+        console.error("Error signing out: ", error);
+      });
+  };
 
-        ':hover': {
-            backgroundColor: 'red',  // red on hover
-            color: 'white',
-            textAlign: 'center',
-            justifyContent:'center',
-        },
-    }));
+  // styling za logout gumb
+  const LogoutButton = styled(MenuItem)(() => ({
+    backgroundColor: "rgba(255, 24, 24, 0.27)",
+    color: "black",
+    textAlign: "center",
+    justifyContent: "center",
 
-    if (!currentUser) return null;
+    ":hover": {
+      backgroundColor: "red",
+      color: "white",
+      textAlign: "center",
+      justifyContent: "center",
+    },
+  }));
 
-    return (
-      <LogoutButton className="logout_button" onClick={handleSignOut}>Sign Out</LogoutButton>
-    );
+  if (!currentUser) return null;
+
+  return (
+    <LogoutButton className="logout_button" onClick={handleSignOut}>Sign Out</LogoutButton>
+  );
+};
+
+SignOutButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  onClose: PropTypes.bool.isRequired,
 };
 
 export default SignOutButton;
